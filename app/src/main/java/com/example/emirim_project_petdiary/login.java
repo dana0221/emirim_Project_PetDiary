be96bfe17b1ff02ca88898b1c9bc762144dd31cd
dana8 <w2008@e-mirim.hs.kr>
2021-07-27 15:36:44 +0900

    Edit login Commit

--- app/src/main/java/com/example/emirim_project_petdiary/login.java
+++ app/src/main/java/com/example/emirim_project_petdiary/login.java
@@ -23,4 +23,21 @@ public class login extends AppCompatActivity {
         Button btnDoan = findViewById(R.id.btn_done);
         btnDoan.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+
+            if(editId.getText().toString().replace(" ","").equals("")){
+                Toast.makeText(v.getContext(), "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+            }else if(editPw.getText().toString().replace(" ","").equals("")){
+                Toast.makeText(v.getContext(), "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "로그인 완료", Toast.LENGTH_SHORT).show();
+                intent = new Intent(getApplicationContext(), home.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
